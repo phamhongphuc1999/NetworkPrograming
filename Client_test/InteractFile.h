a2e53f0979bfec49f336cc5751040c87d3ebc2d7
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2020-06-08 05:54:14 +0700

    add: Encapsulate file and Decapsulate file

--- Client_test/InteractFile.h
+++ Client_test/InteractFile.h
@@ -1,13 +1,17 @@
 #pragma once
 #include "CONST.h"
+#include "InputAndData.h";
 #include <fstream>
 #include <list>
 #include <string>
 #include <windows.h>
 using namespace std;
 
+char* EncapsulatePayload(char* id, string fileName, string data);
+char* DecapsulatePayload(char* id, char* fileName, char* buff);
 bool IsFileExistOrValid(string pathToFile);
 list<string> CreatePayload(string pathToFile);
+list<char*> CreatePayload(char* id, string pathToFile);
 list<string> read_directory(const string& pathToFolder);
 bool SearchFileInDirectory(const string& pathToFolder, string fileName);
 string GetFileName(const string& str);
